a79df7b81bcf138b958fbd4d6bef95ac2d145b32
jdcourcol <jdcourcol@github.com>
2017-06-24 14:33:51 +0200

    initial implementation with F1 environment

---
+++ examples/cem_f1.py
@@ -0,0 +1,58 @@
+''' RL on F1 boardgame with CEM agent '''
+import logging
+import numpy as np
+import gym
+
+from keras.models import Sequential
+from keras.layers import Dense, Activation, Flatten
+
+from rl.agents.cem import CEMAgent
+from rl.memory import EpisodeParameterMemory
+
+ENV_NAME = 'F1-v0'
+
+logging.basicConfig(level=logging.DEBUG)
+
+# Get the environment and extract the number of actions.
+env = gym.make(ENV_NAME)
+np.random.seed(123)
+env.seed(123)
+
+nb_actions = env.action_space.n
+obs_dim = env.observation_space.shape[1]
+# Option 1 : Simple model
+model = Sequential()
+#model.add(Flatten(input_shape=(obs_dim,)))  #
+
+model.add(Flatten(input_shape=(1, ) + (obs_dim, )))
+model.add(Dense(16))
+model.add(Activation('relu'))
+model.add(Dense(16))
+model.add(Activation('relu'))
+model.add(Dense(16))
+model.add(Activation('relu'))
+model.add(Dense(nb_actions))
+model.add(Activation('softmax'))
+
+print(model.summary())
+
+# Finally, we configure and compile our agent. You can use every built-in Keras optimizer and
+# even the metrics!
+memory = EpisodeParameterMemory(limit=1000, window_length=1)
+cem = CEMAgent(
+    model=model,
+    nb_actions=nb_actions,
+    memory=memory,
+    batch_size=50,
+    nb_steps_warmup=2000,
+    train_interval=50,
+    elite_frac=0.05)
+cem.compile()
+
+cem.fit(env, nb_steps=1000000, visualize=False, verbose=1)
+
+# After training is done, we save the best weights.
+cem.save_weights('cem_{}_params.h5f'.format(ENV_NAME), overwrite=True)
+
+# Finally, evaluate our algorithm for 5 episodes.
+cem.test(env, nb_episodes=5, visualize=True)
